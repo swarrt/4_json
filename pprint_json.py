6a87a6c7aaf5f068ae3167923f2f846ebc61b53f
Swarrt <swarrt@gmail.com>
2017-12-29 12:26:46 +0300

    написаны функции чтения из json файла и вывод содержимого файла в удобочитаемом виде

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,15 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding='utf-8') as fp:
+        data = json.load(fp)
+
+    return data
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, sort_keys=True, ensure_ascii = False, indent=4)
 
 
 if __name__ == '__main__':
-    pass
+    print(pretty_print_json(load_data('alco_shops.json')))
